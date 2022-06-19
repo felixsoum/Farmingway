dedef3ff9dffee0c03ad296882c5c313dad9df63
Felix <felix@felixsoum.com>
2022-06-18 23:23:20 -0400

    Implement base connect to server feature

--- Farmingway/Program.cs
+++ Farmingway/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Discord;
+using Discord.WebSocket;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +10,36 @@ namespace Farmingway
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static Task Main(string[] args) => new Program().MainAsync();
+
+        private DiscordSocketClient _client;
+
+        public async Task MainAsync()
         {
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+
+            //  You can assign your bot token to a string, and pass that in to connect.
+            //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
+            var token = Secret.DiscordToken;
+
+            // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
+            // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
+            // var token = File.ReadAllText("token.txt");
+            // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
     }
 }
