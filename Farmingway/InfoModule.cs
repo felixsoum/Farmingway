f4a43599525692287780d3c4e0d65ccab2945a70
Mio <97194740+itsMiyo@users.noreply.github.com>
2022-06-19 16:29:15 -0400

    Get character data from Discord ping

--- Farmingway/InfoModule.cs
+++ Farmingway/InfoModule.cs
@@ -19,24 +19,54 @@ namespace Farmingway
         // ~char 931843984 -> Xiaofei Li ...
         [Command("char")]
         [Summary("Prints information about a character")]
-        public Task CharAsync([Remainder][Summary("The id of your character")] string charId)
+        public Task CharAsync([Remainder][Summary("The id of your character")] int charId)
         {
             var embed = new EmbedBuilder();
             
             try
             {
-                if (!int.TryParse(charId, out var id))
-                {
-                    throw new Exception("Character ID was not a number");
-                }
-                
-                var character = CollectService.GetCharacter(id);
+                var character = CollectService.GetCharacter(charId);
                 var highestMountID = character.Mounts.IDs[character.Mounts.IDs.Length - 1];
 
                 var mount = CollectService.GetMount(highestMountID);
 
                 embed.WithColor(new Color(0, 255, 0))
-                    .WithTitle($"Character data for ID {id}")
+                    .WithTitle($"Character data for ID {charId}")
+                    .AddField("Name", character.Name)
+                    .AddField("Server", character.Server)
+                    .AddField("Mounts", character.Mounts.Count)
+                    .AddField("Highest Mount", $"{mount.Name} (ID {mount.Id})")
+                    .WithImageUrl(character.Portrait);
+            }
+            catch(Exception e)
+            {
+                Console.Write("ERROR: " + e.Message);
+                embed.WithColor(new Color(255, 0, 0))
+                    .WithTitle("ERROR")
+                    .WithDescription(e.Message);
+            }
+
+            return ReplyAsync(embed: embed.Build());
+        }
+
+        [Command("find")]
+        [Summary("Prints information about a character by Discord username")]
+        public Task FindAsync([Remainder][Summary("The user requested")] IUser user)
+        {
+            var embed = new EmbedBuilder();
+            
+            try
+            {
+                var character = CollectService.GetCharacterFromDiscord(user.Id);
+                var highestMountID = character.Mounts.IDs[character.Mounts.IDs.Length - 1];
+
+                var mount = CollectService.GetMount(highestMountID);
+
+                embed.WithColor(new Color(0, 255, 0))
+                    .WithAuthor(
+                        $"Character data for Discord user {user.Username}#{user.DiscriminatorValue:D4}",
+                        user.GetAvatarUrl(size: 256)
+                    )
                     .AddField("Name", character.Name)
                     .AddField("Server", character.Server)
                     .AddField("Mounts", character.Mounts.Count)
