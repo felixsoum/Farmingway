4f78fc633e252e670dfca2e8b37d933f579f9136
Felix <felix@felixsoum.com>
2022-06-19 01:47:37 -0400

    Format output of char command

---
+++ Farmingway/CharacterResponse.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+
+namespace Farmingway
+{
+    public class CharacterResponse
+    {
+        [JsonProperty(PropertyName = "id")]
+        public int Id { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "server")]
+        public string Server { get; set; }
+
+        [JsonProperty(PropertyName = "portrait")]
+        public string Portrait { get; set; }
+
+        [JsonProperty(PropertyName = "avatar")]
+        public string Avatar { get; set; }
+
+        [JsonProperty(PropertyName = "achievements")]
+        public CharacterResponseAchievements Achievements { get; set; }
+
+        [JsonProperty(PropertyName = "mounts")]
+        public CharacterResponseCount Mounts { get; set; }
+
+        [JsonProperty(PropertyName = "orchestrions")]
+        public CharacterResponseCount Orchestrions { get; set; }
+
+        [JsonProperty(PropertyName = "spells")]
+        public CharacterResponseCount Spells { get; set; }
+
+        [JsonProperty(PropertyName = "emotes")]
+        public CharacterResponseCount Emotes { get; set; }
+
+        [JsonProperty(PropertyName = "bardings")]
+        public CharacterResponseCount Bardings { get; set; }
+
+        [JsonProperty(PropertyName = "hairstyles")]
+        public CharacterResponseCount Hairstyles { get; set; }
+
+        [JsonProperty(PropertyName = "armoires")]
+        public CharacterResponseCount Armoires { get; set; }
+
+        [JsonProperty(PropertyName = "fashions")]
+        public CharacterResponseCount Fashions { get; set; }
+
+        [JsonProperty(PropertyName = "triad")]
+        public CharacterResponseCount Triad { get; set; }
+    }
+
+    public class CharacterResponseAchievements
+    {
+        [JsonProperty(PropertyName = "count")]
+        public int Count { get; set; }
+
+        [JsonProperty(PropertyName = "total")]
+        public int Total { get; set; }
+
+        [JsonProperty(PropertyName = "points")]
+        public int Points { get; set; }
+
+        [JsonProperty(PropertyName = "points_total")]
+        public int PointsTotal { get; set; }
+
+        [JsonProperty(PropertyName = "public")]
+        public bool Public { get; set; }
+
+    }
+
+    public class CharacterResponseCount
+    {
+        [JsonProperty(PropertyName = "count")]
+        public int Count { get; set; }
+
+        [JsonProperty(PropertyName = "total")]
+        public int Total { get; set; }
+
+        [JsonProperty(PropertyName = "ids")]
+        public int[] IDs { get; set; }
+    }
+}
--- Farmingway/InfoModule.cs
+++ Farmingway/InfoModule.cs
@@ -1,31 +1,53 @@
 ﻿using Discord.Commands;
+using RestSharp;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Farmingway
 {
-	// Create a module with no prefix
-	public class InfoModule : ModuleBase<SocketCommandContext>
-	{
-		// ~say hello world -> hello world
-		[Command("say")]
-		[Summary("Echoes a message.")]
-		public Task SayAsync([Remainder][Summary("The text to echo")] string echo)
-			=> ReplyAsync(echo);
+    // Create a module with no prefix
+    public class InfoModule : ModuleBase<SocketCommandContext>
+    {
+        // ~say hello world -> hello world
+        [Command("say")]
+        [Summary("Echoes a message.")]
+        public Task SayAsync([Remainder][Summary("The text to echo")] string echo)
+            => ReplyAsync(echo);
 
-		// ReplyAsync is a method on ModuleBase 
+        // ReplyAsync is a method on ModuleBase 
 
-		// ~char 931843984 -> Xiaofei Li ...
-		[Command("char")]
-		[Summary("Prints information about a character")]
-		public Task CharAsync([Remainder][Summary("The id of your character")] string charId)
+        // ~char 931843984 -> Xiaofei Li ...
+        [Command("char")]
+        [Summary("Prints information about a character")]
+        public Task CharAsync([Remainder][Summary("The id of your character")] string charId)
         {
-			return ReplyAsync("Looking up info for id: " + charId);
+            string request = $"https://ffxivcollect.com/api/characters/{charId}?ids=true";
+
+            try
+            {
+                var client = new RestClient(request);
+                var response = client.Execute<CharacterResponse>(new RestRequest());
+
+                int highestMountID = response.Data.Mounts.IDs[response.Data.Mounts.IDs.Length - 1];
+                var reply = String.Join
+                (
+                    "\n",
+                    $"Name: {response.Data.Name}",
+                    $"Server: {response.Data.Server}",
+                    $"Mounts: {response.Data.Mounts.Count}",
+                    $"Highest Mount ID: {highestMountID}",
+                    response.Data.Portrait
+                );
+
+                return ReplyAsync(reply);
+            }
+            catch(Exception e)
+            {
+                Console.Write("ERROR: " + e.Message);
+                return ReplyAsync("ERROR: " + e.Message);
+            }
         }
 
-		// ReplyAsync is a method on ModuleBase 
-	}
+        // ReplyAsync is a method on ModuleBase 
+    }
 }
